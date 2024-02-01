b9a971f377a702e225cf62e7e6ea9320a5856f70
Vivian Ge <viviange@101.197.dyn.lse.ac.uk>
2024-02-01 14:35:45 +0000

    test

---
+++ get_timetable.py
@@ -0,0 +1,44 @@
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+
+
+base_url = 'https://api.tfl.gov.uk/Line/{line}/Timetable/{station_id}'
+params = {'app_key': api_key,
+          'direction': 'inbound'}
+lines = ['1', '59', '68', '91', '188', '243', 'N1', 'N68', 'N91', 'N171', 'SL6',
+         '9', '23','87', '172', 'N9', 'N44', 'N87', 'N155']
+station_ids = ['490000112M', '490003191F', '490019703Z']
+
+combinations = [(line, station_id) for line in lines for station_id in station_ids]
+
+
+filename = 'timetable.jsonl'
+file_path = os.path.join('data', filename)
+
+
+with open(file_path, 'a') as file:
+    for line, station_id in combinations:
+        url = base_url.format(line=line, station_id=station_id)
+        response = requests.get(url, params=params)
+        
+        if response.status_code == 200:
+            data = response.json()        
+            if isinstance(data, list):
+                for record in data:
+                    if record: 
+                        json_record = json.dumps(record)
+                        file.write(json_record + '\n')
+                    else:
+                        continue
+            else:
+                if data:
+                    json_record = json.dumps(data)
+                    file.write(json_record + '\n')
+        else:
+            print(f"Failed to fetch data for line {line}: HTTP Status Code {response.status_code}")
+
+print("Data has been saved to timetable.jsonl")

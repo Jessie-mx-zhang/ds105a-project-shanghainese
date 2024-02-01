76b2e4808cfd7816885becd7016bfed11b3c0477
shakruii <147736772+shakruii@users.noreply.github.com>
2024-02-01 15:09:34 +0000

    1

--- get_arrivals.py
+++ get_arrivals.py
@@ -13,3 +13,5 @@ params = {'app_key': API_KEY}
 lines = ['1', '59', '68', '91', '188', '243', 'N1', 'N68', 'N91', 'N171', 'SL6',
           '9', '23','87', '172', 'N9', 'N44', 'N87', 'N155']
 station_ids = ['490000112M', '490003191F', '490019703Z']
+
+combinations = [(line, station_id) for station_id in station_ids]
